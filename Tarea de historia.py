c15bb6705d577e37b3a3d8c116751af42cfa75e3
JoendriAlbarran <joendri.albarran.unefa@gmail.com>
2026-09-19 22:40:33 -0400

    si

---
+++ Tarea de historia.py
@@ -0,0 +1,2 @@
+#Historia de resident evil
+print("Historia de resident evil, eres Leon S. Kennedy, un novato que llegó tarde..... Pero temprano para vivir.")
--- hola.py
+++ hola.py
@@ -1,1 +1,19 @@
-hola
+#hola
+
+#practica 1
+
+#hola mundo
+
+print("hola mundo")
+
+print("hola Joendri como estais?")
+
+
+input("cuantos años tienes?")
+
+edad= 20 
+
+print(f"deberias de estar en la universidad ya que tienes {edad} años")
+
+multiplica = 5*5
+print (5*5)
