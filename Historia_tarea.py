c3d80167e59b68201c37f1f3811b4282234c056f
JoendriAlbarran <joendri.albarran.unefa@gmail.com>
2026-09-22 17:37:46 -0400

    si

--- Historia_tarea.py
+++ Historia_tarea.py
@@ -63,14 +63,57 @@ sub_decision1 = input("\nElige una opcion (1 o 2): ")
 if sub_decision1 == "1":
     print ("\nEscalas la reja y entras a la comisaria")
     time.sleep(3)
-    print("\nEntras y ves la comisaria destruida, ves una maquina de escribir y armas en en el mostrador, te equipas")
+    print("\nEntras y ves la comisaria destruida, ves una maquina de escribir y una pistola en en el mostrador, te equipas")
     time.sleep(3)
     print("\nEmpiezas a buscar respuestas investigando la R.P.D. te encuentras a tu supervisor, Marvin Branagh, pero está mordido en el estomago y te dices que te salves tu solo")
     time.sleep(3)
 
 elif sub_decision1 == "2":
     print("\nBuscas otra entrada por el callejon.... Grave error, te atacan los zombis y mueres, Fin del juego master")
+    sys.exit()
+#el comando sys.exit() para cerrar el juego
 
 else: 
     print("\nOpcion no valida. Te quedaste paralizado del susto en la oscuridad y te atacaron los zombis por la espalda y te mueres, fin del juego, gracias por jugar :)")
+    sys.exit()
+#2do escenario sobre otra decision
+
+print("\nLe haces caso a tu supervisor y caminas hacia el cuarto de seguridad...")
+time.sleep(3)
+print("\nAPARECE UN ZOMBI, QUE HACES?")
+time.sleep(3)
+print("\n1. Le disparas con tu pistola")
+print("\n2. Le golpeas con la linterna para ahorrar balas y no alertar a los demas zombis")
+
+sub_decision1 = input("\nElige una opcion (1 o 2): ")
+
+if sub_decision1 == "1":
+    print("\nLe disparas y lo matas, pero empiezan a salir mas zombis, y corres y te salvas, pero te quedan pocas balas")
+    time.sleep(3)
+    print("\nTe diriges al segundo piso, y encuentras una pc, es la pc de tu jefe Irons,ves, que tiene un pendrive en un puerto USB, dice que el virus T se salió de control y la cura está un pendrive de la PC")
+    time.sleep(3)
+    print("\nEscapas y te llevas el pendrive")
+    time.sleep(3)
+    print("\nSaliendo de la R.P.D te encuentras con un peloton del ejercito y les dices que en ese pendrive está la cura.....")
+    time.sleep(3)
+    print("\nLos militares te sacan ileso de Raccon City")
+    time.sleep(3)
+    print("\nFELICIDADES SOBREVIVISTE AL INCIDENTE Y EL GOBIERNO TE RECLUTA EN UN PROGRAMA SECRETO DE LA D.S.O")
+    time.sleep(3)
+    print("\n================GRACIAS POR JUGAR!! ==============")
+    print("Atentamente el alumno Joendri Albarran :)")
+
+elif sub_decision1 == "2":
+        print("\nLe golpeas con la linterna y lo matas, pero empiezan a salir mas zombis y te rodean, empiezan a salir perros tambien...")
+        time.sleep(3)
+        print("\nTe rodean y te comen entre todos, festin para los zombis")
+        time.sleep(2)
+        print("\nGAME OVER")
+        sys.exit()
+
+else:
+     print("\nLo piensas mucho y el zombi te agarra y te muerde el cuello.... Mueres")
+     time.sleep(3)
+     print("\nGAME OVER")
+     sys.exit()
 
